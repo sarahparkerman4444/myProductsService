b3af68f7883cb77bb7bf3a286770ffbc55a2996c
Ralf Kostulski <639859+ralfzen@users.noreply.github.com>
2019-03-21 15:26:38 +0100

    Add urls for ProductCategory-endpoints

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,7 @@ from products import views
 router = routers.SimpleRouter()
 
 router.register(r'products', views.ProductViewSet)
+router.register(r'categories', views.ProductCategoryViewSet)
 router.register(r'property', views.PropertyViewSet)
 
 urlpatterns = router.urls
