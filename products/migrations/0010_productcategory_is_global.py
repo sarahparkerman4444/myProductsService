2ef24e1bcfcc86ff1ca73579bfc74ddfe1c50807
Ralf Kostulski <639859+ralfzen@users.noreply.github.com>
2019-03-25 16:29:29 +0100

    Rename migrations

--- products/migrations/0010_productcategory_is_global.py
+++ products/migrations/0010_productcategory_is_global.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('products', '0009_auto_20190325_0730'),
+        ('products', '0009_product_uuid_primary'),
     ]
 
     operations = [
